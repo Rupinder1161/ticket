dd54fa395e1e29ca8a43dfc2300905b13fa81f05
Gurpreet <gurpreet1161@gmail.com>
2020-04-07 21:52:33 +1200

    Updates

---
+++ precache-manifest.7d9c96d7f34ca4b1d4410d3b890fd864.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d921f9a3896e2b3c5dd4c421d6ca0c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75ea41d12bf2ec996113",
+    "url": "/static/css/2.f387b757.chunk.css"
+  },
+  {
+    "revision": "e13b27b12db3055c1f87",
+    "url": "/static/css/main.a901270d.chunk.css"
+  },
+  {
+    "revision": "75ea41d12bf2ec996113",
+    "url": "/static/js/2.78967ff1.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.78967ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e13b27b12db3055c1f87",
+    "url": "/static/js/main.1ecec6f2.chunk.js"
+  },
+  {
+    "revision": "1d21f18d4a02a84ec9bf",
+    "url": "/static/js/runtime-main.3c83a0d0.js"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/Hel.59389695.svg"
+  },
+  {
+    "revision": "d941a5c342c3ff4587a4d34e11875fbb",
+    "url": "/static/media/Hello.d941a5c3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "db4c87028a0bc51dc1af98bba7745cee",
+    "url": "/static/media/mastercard.db4c8702.png"
+  }
+]);
